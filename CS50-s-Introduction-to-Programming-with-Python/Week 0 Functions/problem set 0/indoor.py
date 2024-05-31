748a44cfae029404bf1af508b8d11018aeb099f7
Jyoti Yadav <72126242+jyotiyadav94@users.noreply.github.com>
2024-05-31 17:15:58 +0200

    Add files via upload

---
+++ CS50-s-Introduction-to-Programming-with-Python/Week 0 Functions/problem set 0/indoor.py
@@ -0,0 +1,9 @@
+def lowercase(input_string):
+    return print(input_string.lower())
+
+def main():
+    greeting = input('Enter the string: \n')
+    lowercase(greeting)
+
+if __name__ == "__main()__:"
+    main()
